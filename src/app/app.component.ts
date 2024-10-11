4d34c1366398a97432870b0bbb62b0eb7041e33b
Chintan <mangukiyachintan2021@gmail.com>
2024-10-11 10:33:53 +0530

    Updated errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostService } from './posts.service';
 
@@ -10,8 +9,7 @@ import { PostService } from './posts.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
-  loadedPosts: Post[] =[];
+  loadedPosts: Post[] = [];
 
   error = null;
 
@@ -21,56 +19,76 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    }, error => {
-      this.isFetching = false;
-      this.error = error.message;
-    });
+    this.postsService.fetchPosts().subscribe(
+      (posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      (error) => {
+        this.isFetching = false;
+        this.error = error.message;
+      }
+    );
 
-    if(this.error)
-    {
-  setInterval(() => {
-    this.onFetchPosts();
-  }, 5000);
-}
-else{
-  setInterval(() => {
-    this.onFetchPosts();
-  }, 10000);
-}
+    if (this.error) {
+      setInterval(() => {
+        this.onFetchPosts();
+      }, 50000);
+    } else {
+      setInterval(() => {
+        this.onFetchPosts();
+      }, 100000);
+    }
   }
 
-  
   onCreatePost(postData: Post) {
-
     this.isFetching = true;
-    this.postsService.createAndStorePost( postData.title, postData.content).subscribe(Response => {
-      this.isFetching = false;
-      this.onFetchPosts();
-    }); 
+    this.postsService
+      .createAndStorePost(postData.title, postData.content)
+      .subscribe((Response) => {
+        this.isFetching = false;
+        this.onFetchPosts();
+      },(error)=>{
+        this.isFetching = false;
+        this.error = error.message;
+      });
   }
 
   onFetchPosts() {
     this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
-      this.isFetching = false;
-      console.log(posts);
-      this.loadedPosts = posts;
-    }, error => {
-      this.loadedPosts = [];
-      this.isFetching = false;
-      this.error = error.message;
-    });
+    this.postsService.fetchPosts().subscribe(
+      (posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      (error) => {
+        this.isFetching = false;
+        this.error = error.message;
+      }
+    );
   }
 
   onClearPosts() {
+    if (
+      confirm(
+        'After clicking ok every posts will be deleted from database too.'
+      )
+    ) {
+      this.isFetching = true;
+      this.postsService.clearPost().subscribe(() => {
 
-    this.isFetching = true;
-    this.postsService.clearPost().subscribe((data)=>{
       this.loadedPosts.length = 0;
+        this.isFetching = false;
+      },
+    (error)=>{
       this.isFetching = false;
-    });
+      this.error = error.message;
+    }
+    );
+    }
+  }
+
+  onHandleError() {
+    this.error = null;
   }
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map } from "rxjs/operators";
@@ -25,8 +25,20 @@ export class PostService{
     }
 
     fetchPosts(){
+
+      let searchParams = new HttpParams();
+
+      searchParams = searchParams.append('print', 'pretty');
+
+      searchParams = searchParams.append('custom', 'key');
+
       return this.http
-      .get<{ [key:string]:Post }>('https://first-app-50ba4-default-rtdb.firebaseio.com/posts.json')
+      .get<{ [key:string]:Post }>('https://first-app-50ba4-default-rtdb.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({ "Custom-Header" : "Hello" }),
+          params: searchParams
+        }
+      )
       .pipe(
         map((responseData) => {
           const postsArray: Post[] = [];
